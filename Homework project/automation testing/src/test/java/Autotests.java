e7c444cff06d7c3f65e2a0a982db2a643262360f
LeonidAntonov <oldos1984@gmail.com>
2021-04-20 10:22:21 +0300

    Поправил замечания по домашнему заданию

--- Homework project/automation testing/src/test/java/Autotests.java
+++ Homework project/automation testing/src/test/java/Autotests.java
@@ -3,6 +3,9 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,7 +16,7 @@ public class Autotests {
     private static WebDriver driver;
 
     @Test
-    public void LogChecker() {
+    public void logChecker() {
         logger.info("Проверяем цвет для инфо");
         logger.error("Проверяем цвет для ошибки");
         logger.fatal("Проверяем цвет для фатальной ошибки");
@@ -22,19 +25,31 @@ public class Autotests {
         logger.debug("Проверяем цвет для дебага");
     }
 
-    @Test
-    public void WebDriverTestAndAssert() {
+    @Before
+    public void startWebDriver(){
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        logger.info("Драйвер успешно запущен");
-        driver.get("https://otus.ru");
-        String title = driver.getTitle();
-        if (StringUtils.isEmpty(title) == false) {
-            logger.info("Название сайта = " + driver.getTitle());
-        }
-        else logger.warn("Не удалось получить информацию о титульной странице");
+        if (driver == null)
+            driver = new ChromeDriver();
 
-        driver.quit();
     }
 
+
+
+    @Test
+    public void webDriverTestAndAssert() {
+        logger.info("Драйвер успешно запущен");
+        driver.get("https://otus.ru");
+        String expectedTitle = "Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(expectedTitle, actualTitle);
+        if (expectedTitle.equals(actualTitle)) {
+            logger.info("Название сайта = {}" ,driver.getTitle());
+        }
+        else logger.warn("Не удалось получить информацию о титульной странице");
+    }
+
+    @After
+    public void quitWebDriver(){
+        driver.quit();
+    }
 }
